3b6ba48f149a7d9a701acef019eb61570c74e83e
roopkt <roopkt@gmail.com>
2017-03-06 12:29:48 -0500

    started adding karma config files

---
+++ sample/karma/karma.conf.js
@@ -0,0 +1,20 @@
+﻿'use strict';
+
+module.exports = function (config) {
+    config.set({
+        autoWatch: true,
+        browsers: ['Chrome', 'PhantomJS'],
+        files: [
+            '../node_modules/es6-shim/es6-shim.min.js',
+            'karma.entry.js'
+        ],
+        frameworks: ['jasmine'],
+        logLevel: config.LOG_INFO,
+        preprocessors: {
+            'karma.entry.js': ['webpack', 'sourcemap']
+        },
+        reporters: ['dots'],
+        singleRun: false
+
+    });
+}
